4ebc82025e9b3ebe9d50df42fcab175a2dd6344e
Benjamin Bergman <benjamin.bergman@humanrights.ca>
2019-05-24 08:33:57 -0500

    Create test app that shows failure to launch rec in compiled version of app

--- renderer.js
+++ renderer.js
@@ -1,3 +1,15 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+var record = require("node-record-lpcm16")
+var fs = require('fs')
+
+setTimeout(function() {
+    var file = fs.createWriteStream('/tmp/test.wav', { encoding: 'binary' })
+
+    record.start({
+      verbose: true
+    })
+    .pipe(file)
+  }, 2000)
